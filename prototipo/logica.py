34070ae6a561c24692c72394ecaf7cf11b35b82a
Josue Barrientos Sandoval <josuebarrientossandoval@gmail.com>
2022-05-31 23:53:07 -0600

    Actulizacion prototipo
    Se junto parte visual con el backend

---
+++ prototipo/logica.py
@@ -0,0 +1,29 @@
+import Stemmer
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+import string
+
+class Indice:
+
+    def __init__(self):
+        self.stopWords = set(stopwords.words('spanish'))
+        self.stemmer = SnowballStemmer("spanish")
+    
+    # Funcion que recibe un criterio de busqueda creando el
+    # indice 
+    def indexar(self, criterioDeBusqueda):
+        textoTokemizado = word_tokenize(criterioDeBusqueda)
+        filtrandoPalabras = [palabra for palabra in textoTokemizado if not palabra in string.punctuation]
+        filtrandoPalabras = [palabra.lower() for palabra in filtrandoPalabras if not palabra in self.stopWords] 
+        return [self.stemmer.stem(palabra) for palabra in filtrandoPalabras]
+
+def main():
+    crearIndice = Indice()
+    listaIndices = crearIndice.indexar("Hola mundo, comiendo para comer")
+    #Buscar()
+    print(listaIndices)
+
+if __name__ == "__main__":
+    main()
